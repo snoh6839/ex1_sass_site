3a99b9add2c54e7314fdbeb512f02796f050bbe0
SubinNoh <snoh6839@mysvc.skagit.edu>
2022-11-23 10:27:11 +0900

    push

--- assets/js/common.js
+++ assets/js/common.js
@@ -103,5 +103,17 @@ $(function(){
     $('td').removeClass('hover');
   });
 
-  
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+
+
 });
